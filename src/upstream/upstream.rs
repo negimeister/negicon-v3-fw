34b12f9090d9cacdc68fc9bf704e352926fc9614
Negimeister <leeklabsinternational@gmail.com>
2023-11-19 16:03:42 +0100

    improve upstream error handling

--- src/upstream/upstream.rs
+++ src/upstream/upstream.rs
@@ -1,12 +1,17 @@
 use rp2040_hal::spi::{SpiDevice, ValidSpiPinout};
-use usb_device::class_prelude::UsbBus;
+use usb_device::{class_prelude::UsbBus, UsbError};
 use usbd_human_interface_device::interface::{InSize, Interface, OutSize, ReportCount};
 
 use crate::negicon_event::NegiconEvent;
 
 use super::spi::SPIUpstream;
 pub(crate) trait Upstream {
-    fn send_event(&mut self, event: &NegiconEvent) -> Result<(), &'static str>;
+    fn send_event(&mut self, event: &NegiconEvent) -> Result<(), UpstreamError>;
+}
+
+pub(crate) enum UpstreamError {
+    SpiError,
+    UsbError(UsbError),
 }
 
 impl<B, I, O, R> Upstream for Interface<'_, B, I, O, R>
@@ -16,19 +21,10 @@ where
     O: OutSize,
     R: ReportCount,
 {
-    fn send_event(&mut self, event: &NegiconEvent) -> Result<(), &'static str> {
+    fn send_event(&mut self, event: &NegiconEvent) -> Result<(), UpstreamError> {
         match self.write_report(&event.serialize()) {
             Ok(_) => Ok(()),
-            Err(e) => match e {
-                usb_device::UsbError::WouldBlock => Err("USB WouldBlock"),
-                usb_device::UsbError::ParseError => Err("USB ParseError"),
-                usb_device::UsbError::BufferOverflow => Err("USB BufferOverflow"),
-                usb_device::UsbError::EndpointOverflow => Err("USB EndpointOverflow"),
-                usb_device::UsbError::InvalidEndpoint => Err("USB InvalidEndpoint"),
-                usb_device::UsbError::InvalidState => Err("USB InvalidState"),
-                usb_device::UsbError::EndpointMemoryOverflow => Err("USB EndpointMemoryOverflow"),
-                usb_device::UsbError::Unsupported => Err("USB Unsupported"),
-            },
+            Err(e) => Err(UpstreamError::UsbError(e)),
         }
     }
 }
@@ -38,7 +34,10 @@ where
     D: SpiDevice,
     P: ValidSpiPinout<D>,
 {
-    fn send_event(&mut self, event: &NegiconEvent) -> Result<(), &'static str> {
-        self.transmit_event(event)
+    fn send_event(&mut self, event: &NegiconEvent) -> Result<(), UpstreamError> {
+        match self.transmit_event(event) {
+            Ok(_) => Ok(()),
+            Err(_) => Err(UpstreamError::SpiError),
+        }
     }
 }
